5e983c6cfbe9b4e8c02c21d37efe9e8a2eeb9d91
Milton Bravo <miltonbravo96@gmail.com>
2021-06-24 18:18:27 -0500

    helper para crear el token

---
+++ API-Framework/Helpers/JWTHelper.cs
@@ -0,0 +1,17 @@
+﻿using API_Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace API_Framework.Helpers
+{
+    public class JWTHelper
+    {
+        public static async Task<string> GenerarToken(Usuario usuario)
+        {
+            return "";
+        }
+    }
+}
