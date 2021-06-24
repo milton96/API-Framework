14ccf153acd4b5d0f7b683117f5b11754041a49b
Milton Bravo <miltonbravo96@gmail.com>
2021-06-24 18:44:15 -0500

    controlador para empleado

---
+++ API-Framework/Controllers/_EmpleadoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace API_Framework.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/empleado")]
+    public class _EmpleadoController : ApiController
+    {
+        [HttpPost]
+        [Route("ping")]
+        public async Task<IHttpActionResult> Ping()
+        {
+            try
+            {
+                var a = User;
+                object res = new
+                {
+                    estatus = "En sesión"
+                };
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
