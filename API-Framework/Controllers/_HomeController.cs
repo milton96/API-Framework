a9358b427237120b8a479124667567c9a1736365
Milton Bravo <miltonbravo96@gmail.com>
2021-06-24 16:06:58 -0500

    el ping publico ahora retorna información sobre la conexión a la base de datos

--- API-Framework/Controllers/_HomeController.cs
+++ API-Framework/Controllers/_HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using API_Framework.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace API_Framework.Controllers
@@ -13,9 +15,24 @@ namespace API_Framework.Controllers
     {
         [HttpPost]
         [Route("ping")]
-        public IHttpActionResult Ping()
+        public async Task<IHttpActionResult> Ping()
         {
-            return Ok("API Up");
+            try
+            {
+                bool db_ok = await Conexion.TestConexion();
+                string msj_db = db_ok ? "Conexión exitosa" : "Ocurrió un problema al realizar la conexión.";
+
+                object res = new
+                {
+                    db = msj_db,
+                    estatus = "API en línea"
+                };
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
