afb0318cb54ea1541b1a8195bcb1f1c113a901b8
Milton Bravo <miltonbravo96@gmail.com>
2021-06-24 17:55:44 -0500

    Clase para extensiones

---
+++ API-Framework/Helpers/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace API_Framework.Helpers
+{
+    public static class Extensions
+    {
+
+        public static T GetValor<T>(this SqlDataReader reader, int index)
+        {
+            return reader.IsDBNull(index) ? default : (T)reader.GetValue(index);
+        }
+
+        public static object ToDB(this String value)
+        {
+            return String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value) ? DBNull.Value : (object)value;
+        }
+
+        public static object ToDB(this int value)
+        {
+            return value <= 0 ? DBNull.Value : (object)value;
+        }
+    }
+}
