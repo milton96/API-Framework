5cf4891caa8ed3f608d9790dac328eeef10f9977
Milton Bravo <miltonbravo96@gmail.com>
2021-06-24 16:13:00 -0500

    agregados modelos rol y usuario

---
+++ API-Framework/Models/Rol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_Framework.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Codigo { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ API-Framework/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_Framework.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public Rol IdRol { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+        public bool Activo { get; set; }
+    }
+}
