966192aa7f2d8964a132627a16c70a860156352e
Milton Bravo <miltonbravo96@gmail.com>
2021-06-24 16:25:41 -0500

    Helper para errores

---
+++ API-Framework/Helpers/ErrorHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_Framework.Helpers
+{
+    public class ErrorHelper
+    {
+        private List<string> Errors { get; set; }
+        public bool HasErrors 
+        { 
+            get
+            {
+                return Errors.Any();
+            } 
+        }
+
+        public ErrorHelper()
+        {
+            Errors = new List<string>();
+        }
+
+        public void Add(string mensaje)
+        {
+            Errors.Add(mensaje);
+        }
+
+        public List<string> GetErrors()
+        {
+            return Errors;
+        }
+    }
+}
