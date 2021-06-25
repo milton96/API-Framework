f3fdfebc3ae7e84c138ebc8c09cccf83f8bf6ce5
Milton Bravo <miltonbravo96@gmail.com>
2021-06-25 18:58:06 -0500

    agregado controlador para productos

---
+++ API-Framework/Controllers/_ProductoController.cs
@@ -0,0 +1,19 @@
+﻿using API_Framework.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+
+namespace API_Framework.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/producto")]
+    public class _ProductoController : ApiController
+    {
+        
+    }
+}
