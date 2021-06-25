8c7a6fd60aab0fbda4d071698edc4c2c2cc43e2f
Milton Bravo <miltonbravo96@gmail.com>
2021-06-25 18:54:41 -0500

    agregado modelo Producto

---
+++ API-Framework/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_Framework.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Codigo { get; set; }
+        public int Stock { get; set; }
+        public string Imagen { get; set; }
+        public Usuario CreadoPor { get; set; }
+        public Usuario ModificadoPor { get; set; }
+        public DateTime Creado { get; set; }
+        public DateTime Modificado { get; set; }
+        public bool Activo { get; set; }
+    }
+}
