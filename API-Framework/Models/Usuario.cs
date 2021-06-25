a5bd52754586e9cca9571ecfb78e73f075a831d8
Milton Bravo <miltonbravo96@gmail.com>
2021-06-24 20:08:25 -0500

    obtener usuario por id

--- API-Framework/Models/Usuario.cs
+++ API-Framework/Models/Usuario.cs
@@ -202,5 +202,74 @@ namespace API_Framework.Models
 
             return usuarios;
         }
+
+        public static async Task<Usuario> ObtenerPorId(int id)
+        {
+            Usuario u = null;
+            try
+            {
+                string query = @"SELECT U.*, R.Nombre AS NombreRol, R.Codigo AS CodigoRol, R.Activo AS ActivoRol FROM [dbo].[Usuario] U
+	                                INNER JOIN  [dbo].[Rol] R ON R.Id = U.IdRol
+                                WHERE U.Id = @ID";
+                using (SqlConnection con = Conectar())
+                {
+                    using (SqlCommand command = new SqlCommand(query, con)
+                    {
+                        CommandType = CommandType.Text,
+                        CommandTimeout = 60
+                    })
+                    {
+                        command.Parameters.AddWithValue("@ID", id);
+                        con.Open();
+                        using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                        {
+                            var i = new
+                            {
+                                Id = reader.GetOrdinal("Id"),
+                                Rol = new
+                                {
+                                    Id = reader.GetOrdinal("IdRol"),
+                                    Nombre = reader.GetOrdinal("NombreRol"),
+                                    Codigo = reader.GetOrdinal("CodigoRol"),
+                                    Activo = reader.GetOrdinal("ActivoRol")
+                                },
+                                Nombre = reader.GetOrdinal("Nombre"),
+                                ApellidoPaterno = reader.GetOrdinal("ApellidoPaterno"),
+                                ApellidoMaterno = reader.GetOrdinal("ApellidoMaterno"),
+                                Correo = reader.GetOrdinal("Correo"),
+                                Password = reader.GetOrdinal("Password"),
+                                Activo = reader.GetOrdinal("Activo")
+                            };
+
+                            if (reader.Read())
+                            {
+                                u = new Usuario();
+                                u.Id = reader.GetValor<int>(i.Id);
+                                u.Nombre = reader.GetValor<string>(i.Nombre);
+                                u.ApellidoPaterno = reader.GetValor<string>(i.ApellidoPaterno);
+                                u.ApellidoMaterno = reader.GetValor<string>(i.ApellidoMaterno);
+                                u.Correo = reader.GetValor<string>(i.Correo);
+                                //u.Password = reader.GetValor<string>(i.Password);
+                                u.Activo = reader.GetValor<bool>(i.Activo);
+                                u.IdRol = new Rol()
+                                {
+                                    Id = reader.GetValor<int>(i.Rol.Id),
+                                    Nombre = reader.GetValor<string>(i.Rol.Nombre),
+                                    Codigo = reader.GetValor<int>(i.Rol.Codigo),
+                                    Activo = reader.GetValor<bool>(i.Rol.Activo)
+                                };
+                            }
+                        }
+                        con.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return u;
+        }
     }
 }
