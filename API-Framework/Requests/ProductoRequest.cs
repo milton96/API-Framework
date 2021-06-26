c404525fe619c16fccf3310e7bcc46e3d398d28a
Milton Bravo <miltonbravo96@gmail.com>
2021-06-25 19:21:52 -0500

    agregado request para producto

---
+++ API-Framework/Requests/ProductoRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace API_Framework.Requests
+{
+    public class ProductoRequest
+    {
+        [Required(ErrorMessage = "El nombre del producto es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El precio del producto es requerido")]
+        public decimal Precio { get; set; }
+        [Required(ErrorMessage = "El código del producto es requerido")]
+        public string Codigo { get; set; }
+        [Required(ErrorMessage = "La cantidad de productos es requerida")]
+        public int Stock { get; set; }
+        [Required(ErrorMessage = "La imagen del producto es requerida")]
+        public string Imagen { get; set; }
+        public bool Activo { get; set; }
+    }
+}
